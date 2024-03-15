16f9ee623451b1565aca03869931adfe41ca6b92
Mefdev <mefdev.pro@gmail.com>
2024-03-15 00:40:47 +0000

    Add all

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """types of the elements of the input are not known"""
 
-from typing import List
+from typing import Tuple, List
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List = [
         item for item in lst
         for _ in range(factor)
     ]
-    return zoomed_in * int(factor)
+    return zoomed_in
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """sum float numbers in a list"""
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum float numbers in a list"""
     return sum(mxd_lst)
